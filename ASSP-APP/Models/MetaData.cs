34f06113a4599749ec89ef5627fd37c5154d6a13
idrees <idreesnazir786@gmail.com>
2017-12-27 00:29:14 +0500

    Added Classes

---
+++ ASSP-APP/Models/MetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ASSP_APP.Models
+{
+
+    public class UserMetaData
+    {
+        
+    }
+    
+        
+    
+}
---
+++ ASSP-APP/Models/PartialClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASSP_APP.Models
+{
+
+    [MetadataType(typeof(UserMetaData))]
+    public partial class User
+    {
+
+    }
+
+    
+}
