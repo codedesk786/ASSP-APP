f5ddf27ce188129f9d58a27c8307bae17b4cbb58
Muddassir Irshad <mudassir.irshad@logicpowered.net>
2017-12-29 23:22:11 +0500

    Add user

--- ASSP-APP/Controllers/UserController.cs
+++ ASSP-APP/Controllers/UserController.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Google.Authenticator;
+using ASSP_APP.Models;
 
 namespace ASSP_APP.Controllers
 {
     public class UserController : Controller
     {
+        private AsspDB db = new AsspDB();
         // GET: User
-        public ActionResult Index()
+        public ActionResult adduser()
         {
             return View();
         }
-        public ActionResult adduser()
+        public ActionResult InsertUser(string FullName, string UserName,string Password,string Address,int RoleID)
         {
+            if(RoleID != null)
+            {
+                User objUser = new User();
+                objUser.FullName = FullName;
+                objUser.UserName = UserName;
+                objUser.Password = Password;
+                objUser.Address1 = Address;
+                objUser.RolID = RoleID;
+                db.Users.Add(objUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
--- ASSP-APP/Models/LoginModel.cs
+++ ASSP-APP/Models/LoginModel.cs
@@ -7,10 +7,16 @@ namespace ASSP_APP.Models
 {
     public class LoginModel
     {
+        public string FullName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Address { get; set; }
         public bool RememberMe { get; set; }
         public bool Enable2fact { get; set; }
+
+
+
+
        
     }
 }
